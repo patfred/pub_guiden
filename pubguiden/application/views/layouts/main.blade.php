603f3e012b7f08b8db10165e64beb88c0d96e989
Daniel Lindstedt <lindhara@gmail.com>
2013-01-30 15:55:28 +0100

    Grundinstallation Laravel
    Innehåller bootrap script och css

---
+++ pubguiden/application/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pubguiden</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap-responsive.css"></link>
+        <!-- {{ Asset::styles() }}
+        {{ Asset::scripts() }} -->
+    </head>
+    <body>
+        Hello World
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
