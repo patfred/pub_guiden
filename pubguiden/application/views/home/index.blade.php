83a1f9f6660a40079aa4b5fe74c35dd97ed81c55
Caroline Norin <carolinenorin@Caroline-Norins-MacBook-Pro.local>
2013-02-04 23:30:48 +0100

    utkast design, bob bundle
    Grundläggande styling av startsidan. Installerade bob för eventuell
    scaffolding. Bootstrapper som bundle fungerade inte. Script och css
    laddas traditionellt pga att assets inte fungerade

--- pubguiden/application/views/home/index.blade.php
+++ pubguiden/application/views/home/index.blade.php
@@ -1,7 +1,33 @@
 @layout('layouts/main')
+@section('content')
+	<section class="content">	
+		<div class="home-menu content-holder">   
+			<input type="text" class="home-search search-query" placeholder="Sök" > 
+			<a href="#"><i class="icon-search icon-large"></i> </a> 
+		</div>
+		<a href="#">
+			<div class="home-menu content-holder">  Topp 20  
+				<i class="icon-angle-right icon-large"></i> 
+			</div>
+		</a>
+		<a href="#">
+			<div class="home-menu content-holder">  Pubar nära dig  
+				<i class="icon-angle-right icon-large"></i> 
+			</div>
+		</a>
+		<a href="#">
+			<div class="home-menu content-holder"> Kategorier
+				<i class="icon-angle-right icon-large"></i> 
+			</div>
+		</a>
+	</section>
 
-<dl>
-	@foreach($pubs as $pub)
-		    <dt>{{ HTML::link_to_action("pub@view", $pub->name, array($pub->id)) }} </dt>
-	@endforeach
-</dl>
+	<div class="content-holder"> 
+		<dl>
+			@foreach($pubs as $pub)
+				<dt> <i class="icon-map-marker"></i>{{ HTML::link_to_action("pub@view", $pub->name, array($pub->id)) }} </dt>
+			@endforeach
+		</dl>
+	</div>	
+	
+@endsection
--- pubguiden/application/views/layouts/main.blade.php
+++ pubguiden/application/views/layouts/main.blade.php
@@ -5,10 +5,28 @@
         <title>Pubguiden</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="css/bootstrap-responsive.css"></link>
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
         <!-- {{ Asset::styles() }}
         {{ Asset::scripts() }} -->
     </head>
     <body>
+
+        <header class="menu-top navbar navbar-fixed-top">
+        @section('navigation')
+                <a href="#" class="logo">
+                    <i class="icon-beer icon-large"></i> Pubguiden Gbg
+                </a>
+                <a href="#" class="menu-icons"> <i class="icon-search icon-2x"></i></a>
+                <a href="#" class="menu-icons"> <i class="icon-user icon-2x"></i></a>
+         @yield_section
+        </header> 
+
+        @yield('content') 
+        
+        <footer>
+            <p> &copy; Pubguiden 2013</p>
+        </footer>
+
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="js/bootstrap.min.js"></script>
     </body>
