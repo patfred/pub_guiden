089047fa4f6ca5988fc2c3e72a6b3da253855e3e
Karin <karintoft@hotmail.com>
2013-02-04 12:33:47 +0100

    views, klasser och controllers försök två

--- pubguiden/application/models/Pub.php
+++ pubguiden/application/models/Pub.php
@@ -1,13 +1,8 @@
 <?php
-<<<<<<< HEAD
+
 
 class Pub extends Eloquent {
 	public static $table = 'pubs';
 }
-=======
-	class Pub extends Eloquent {
 
-
-
-	} 
->>>>>>> de1b1932ed4a70a5c6a7bd256322ab89056275e1
+	
--- pubguiden/application/views/pubs/index.blade.php
+++ pubguiden/application/views/pubs/index.blade.php
@@ -1,30 +1,7 @@
 @layout('layouts/main')
 
-<<<<<<< HEAD
+
 <p>
 	Här är pubs vyn
 </p>
-=======
-<h1>Detta är pubs/index</h1>
-<dl>
-@foreach($pubs as $pub)
-	    <dt>{{ HTML::link_to_action("pub@view", $pub->name, array($pub->id)) }} </dt>
-	    <dd>{{ $pub->description }} </dd>
-	    <dd>Adress: {{ $pub->address}}</dd>
-	    <dd>Billigaste öl: {{ $pub->lowest_price }} kr</dd>
-	    <dd>Quiz? @if($pub->quiz == 0)
-	    			Nej
-	    			@else
-	    			Ja
-	    		@endif</dd>
-		<dd>After Work? @if($pub->after_work == 0)
-	    			Nej
-	    			@else
-	    			Ja
-	    		@endif</dd>
-	@endforeach
-</dl>
 
-
-
->>>>>>> de1b1932ed4a70a5c6a7bd256322ab89056275e1
