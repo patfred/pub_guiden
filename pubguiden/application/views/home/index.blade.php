7eb71da7c6ca542ee4484d6931ccf1fa4285c3e0
Karin <karintoft@hotmail.com>
2013-02-07 13:38:13 +0100

    Klickbara <div>ar på startsida

--- pubguiden/application/views/home/index.blade.php
+++ pubguiden/application/views/home/index.blade.php
@@ -5,25 +5,30 @@
 			<input type="text" class="home-search search-query" placeholder="Sök" > 
 			<a href="#"><i class="icon-search icon-large"></i> </a> 
 		</div>
-			<div class="home-menu content-holder">  {{ HTML::link_to_action("results@top20", "Topp 20") }}  
-				<i class="icon-angle-right icon-large"></i> 
+		<a href="<?php echo URL::to_action('results@top20') ?>">
+			<div class='home-menu content-holder'>
+				Topp 20
+				<i class="icon-angle-right icon-large"></i>
 			</div>
+		</a>		
 		<a href="#">
-			<div class="home-menu content-holder">  Pubar nära dig  
-				<i class="icon-angle-right icon-large"></i> 
+			<div class='home-menu content-holder'>
+				Pubar nära dig
+				<i class="icon-angle-right icon-large"></i>
 			</div>
 		</a>
-			<div class="home-menu content-holder"> {{ HTML::link_to_action("categories@index", "Kategorier") }}  
-				<i class="icon-angle-right icon-large"></i> 
+		<a href="<?php echo URL::to_action('categories@index') ?>">
+			<div class='home-menu content-holder'>
+				Kategorier
+				<i class="icon-angle-right icon-large"></i>
 			</div>
+		</a>	
 	</section>
-
 	<div class="content-holder"> 
 		<dl>
 			@foreach($pubs as $pub)
 				<dt> <i class="icon-map-marker"></i>{{ HTML::link_to_action("pubs@index", $pub->name, array($pub->id)) }} </dt>
 			@endforeach
 		</dl>
-	</div>	
-	
+	</div>		
 @endsection
