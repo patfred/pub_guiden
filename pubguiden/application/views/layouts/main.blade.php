e7c597e9c53146ffb465b03f02685de7d7ccabbd
Caroline Norin <carolinenorin@Caroline-Norins-MacBook-Pro.local>
2013-02-07 01:43:47 +0100

    Drop-down för sök och login
    Lade till app.js för dropdown, justerade padding top för content när
    top-baren inte är "fixed".

--- pubguiden/application/views/layouts/main.blade.php
+++ pubguiden/application/views/layouts/main.blade.php
@@ -13,11 +13,23 @@
 
         <header class="menu-top navbar navbar-fixed-top">
         @section('navigation')
-                <span class="logo">
-                    <i class="icon-beer icon-large"></i> {{ HTML::link_to_action("home", "Pubguiden Gbg") }}  
-                </span>
-                <a href="#" class="menu-icons"> <i class="icon-search icon-2x"></i></a>
-                <a href="#" class="menu-icons"> <i class="icon-user icon-2x"></i></a>
+            <span class="logo">
+                <i class="icon-beer icon-large"></i> {{ HTML::link_to_action("home", "Pubguiden Gbg") }}  
+            </span>
+            
+            <a href="#" id="search-drop" class="menu-icons"> <i class="icon-search icon-2x"></i></a>
+            <a href="#" id="login-drop" class="menu-icons"> <i class="icon-user icon-2x"></i></a>
+            <div class="menu-box">
+                <div id="show-search" class="show-hide"> 
+                    <input type="text" class="home-search search-query" placeholder="Sök" > 
+                    <a href="#"><i class="icon-search icon-large"></i> </a> 
+                </div>
+                <div id="show-login" class="show-hide"> 
+                    <input type="text" class="facebook-login" placeholder="Användarnamn" > 
+                    <input type="password" class="facebook-passw" placeholder="Lösenord" > 
+                </div>
+            </div>
+    
          @yield_section
         </header> 
 
@@ -29,5 +41,7 @@
 
     <?php echo HTML::script('http://code.jquery.com/jquery-latest.js');?>
     <?php echo HTML::script('js/bootstrap.min.js');?>   
+    <?php echo HTML::script('js/app.js');?>   
+
     </body>
 </html>
