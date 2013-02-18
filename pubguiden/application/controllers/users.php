fbb31bb5a9b4ad6fdec43c0f059c1253b3fd743b
Caroline Norin <carolinenorin@Caroline-Norins-MacBook-Pro.local>
2013-02-18 11:41:12 +0100

    Tagit bort user controller och model som inte används

--- pubguiden/application/controllers/users.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Users_Controller extends Base_Controller {
-
-
-}
--- pubguiden/bundles/myauth/models/user.php
+++ pubguiden/bundles/myauth/models/user.php
@@ -8,4 +8,14 @@
 
 class User extends Eloquent {
 	
+	public function comment()
+	{
+		return $this->has_many('Comment');
+	}	
+
+	public function rating()
+	{
+		return $this->has_many('Rating');
+	}
+	
 }
