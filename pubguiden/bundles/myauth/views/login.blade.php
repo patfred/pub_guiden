21ee1bc0515fe85558d355f04e29bc7f61568191
Caroline Norin <carolinenorin@Caroline-Norins-MacBook-Pro.local>
2013-02-19 12:17:45 +0100

    finjusteringar

--- pubguiden/bundles/myauth/views/login.blade.php
+++ pubguiden/bundles/myauth/views/login.blade.php
@@ -5,13 +5,8 @@
 <h2>Logga in på pubguiden</h2>
 
 {{ Form::open(Config::get('myauth::config.bundle_route') . '/' . Config::get('myauth::config.login_route')) }}
-    <!-- check for login errors flash var -->
-  
-    @if (Session::has('notification'))
-        <span class="notification">{{ Session::get('notification') }}</span>
-    @endif
     
-    <!-- username field -->
+    <!-- user email field -->
     <span>{{ Form::label('email', 'E-mail') }}</span>
     <p>{{ Form::text('email') }}</p>
     
@@ -19,9 +14,15 @@
     <span>{{ Form::label('password', 'Lösenord') }}</span>
     <p>{{ Form::password('password') }}</p>
     
+    <!-- check for login errors flash var -->
+    @if (Session::has('notification'))
+        <p class="notification"> <i class='icon-warning-sign'></i>{{ Session::get('notification') }}</p>
+    @endif
+
     <!-- submit button -->
     <p>{{ Form::submit('Logga in') }}</p>
+
 {{ Form::close() }}
 
-@endsection
 </section> 
+@endsection
--- pubguiden/bundles/myauth/views/signup.blade.php
+++ pubguiden/bundles/myauth/views/signup.blade.php
@@ -2,31 +2,31 @@
 @section('content')
 <section class="content container login-signup">
 
-<h2>Registrera dig!</h2>
-<p>För att kunna betygsätta och kommentera pubar behöver du registrera dig! </p> 
-<p>Observera att lösenordet måste innehålla minst 5 tecken.</p>
+    <h2>Registrera dig!</h2>
+    <p>För att kunna betygsätta och kommentera pubar behöver du registrera dig! </p> 
+    <p>Observera att lösenordet måste innehålla minst 5 tecken.</p>
 
-{{ Form::open(Config::get('myauth::config.bundle_route') . '/' . Config::get('myauth::config.signup_route')) }}
+    {{ Form::open(Config::get('myauth::config.bundle_route') . '/' . Config::get('myauth::config.signup_route')) }}
 
-    <!-- email field -->
-    <span>{{ Form::label('email', 'E-mail') }}</span>
-    <p>{{ Form::text('email', Input::old('email'), array('class' => 'well')) }}</p>
-    <?php if($errors) echo '<p class="error">'.$errors->first('email').'</p>'; ?>
+        <!-- email field -->
+        <span>{{ Form::label('email', 'E-mail') }}</span>
+        <p>{{ Form::text('email', Input::old('email'), array('class' => 'well')) }}</p>
+            <?php if($errors) echo '<p class="error">'.$errors->first('email').'</p>'; ?>
+        
+        <!-- password field -->
+        <span>{{ Form::label('password', 'Lösenord') }}</span>
+        <p>{{ Form::password('password', array('class' => 'well')) }}</p>
+            <?php if($errors) echo '<p class="error">'.$errors->first('password').'</p>'; ?>
+
+        <!-- confirm password field -->
+        <span>{{ Form::label('password_confirmation', 'Bekräfta lösenord') }}</span>
+        <p>{{ Form::password('password_confirmation', array('class' => 'well')) }}</p>
+            <?php if($errors) echo '<p class="error">'.$errors->first('password_confirmation').'</p>'; ?>
+
+        <!-- submit button -->
+        <p>{{ Form::submit('Registrera dig', array('class' => 'signup', 'onclick' => 'return validate()')) }}</p>
     
-    <!-- password field -->
-    <span>{{ Form::label('password', 'Lösenord') }}</span>
-    <p>{{ Form::password('password', array('class' => 'well')) }}</p>
-    <?php if($errors) echo '<p class="error">'.$errors->first('password').'</p>'; ?>
+    {{ Form::close() }}
 
-    <!-- confirm password field -->
-    <span>{{ Form::label('password_confirmation', 'Bekräfta lösenord') }}</span>
-    <p>{{ Form::password('password_confirmation', array('class' => 'well')) }}</p>
-    <?php if($errors) echo '<p class="error">'.$errors->first('password_confirmation').'</p>'; ?>
-
-    <!-- submit button -->
-    <p>{{ Form::submit('Registrera dig', array('class' => 'signup', 'onclick' => 'return validate()')) }}</p>
-    
-{{ Form::close() }}
-
-@endsection
 </section>
+@endsection
