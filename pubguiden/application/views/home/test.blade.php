9eb12a4a2429d614f4c42be88ff6e788798fd9b8
Daniel Lindstedt <lindhara@gmail.com>
2013-02-27 15:13:34 +0100

    Lekt med Jsonobject och geolocation. I nearestvyn.

--- pubguiden/application/views/home/test.blade.php
+++ pubguiden/application/views/home/test.blade.php
@@ -4,20 +4,16 @@
 		<div id="json_test">
 
 		</div>
-		
 	</section>	
 @endsection
 @section('scripts')
 	<script>
 		$.get('json', function(result){
-			
 			for(var i=0;  i < result.length ;i++){
-				
-					$('#json_test').append( "<p>" + result[i].attributes.name + "</p>");
-				
+					$('#json_test').append( "<p>" + result[i].attributes.longitude + "</p>");
+					$('#json_test').append( "<p>" + result[i].attributes.latitude + "</p>");
 				}
 		});
-
 	</script>
 @endsection
 	
--- pubguiden/application/views/list_pubs/nearestPubs.blade.php
+++ pubguiden/application/views/list_pubs/nearestPubs.blade.php
@@ -5,10 +5,31 @@
 	<h2>Pubar nära dig</h2>
 	<p>Din position:	
 	<p id="demo"></p>
+	<p id="json_test"></p>
 	<ul>
 		@foreach($pubs as $pub)
-			<li>{{ HTML::link_to_action("pubs@index", $pub->name, array($pub->id)) }} </li>
+			<li>{{ HTML::link_to_action("pubs@index", $pub->name, array($pub->id)) }}<span id="pub_<?php echo $pub->id; ?>"></span></li>
 		@endforeach
 	</ul>
 </section>
+@endsection
+@section('scripts')
+	<script>
+		$.get('http://localhost/pub_guiden/pubguiden/public/home/json', function(result){
+		if( sessionStorage.lat ){
+			for(var i=1; i<result.length; i++){
+				$('#pub_' + i).append("<p>Du är X meter ifrån " + result[i].attributes.name + "</p>")
+					.append('<p>Lat:' + sessionStorage.lat + '</p>')
+					.append('<p>Long:' + sessionStorage.lng + '</p>');
+			}
+		} else { 
+			$('#json_test').append("<p>No session storage found!</p>");
+		}
+			// for(var i=0; i<result.length; i++){
+			// 		pub_lat = result[i].attributes.latitude;
+			// 		pub_long = result[i].attributes.longitude;
+			// 		$('#json_test').append( "<p>" + pub_long + "</p>").append( "<p>" + pub_lat + "</p>");
+			// 	}
+		});
+	</script>
 @endsection
