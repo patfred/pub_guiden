352095da503bb74c24883458256bbc292ed3f44c
Caroline Norin <carolinenorin@Caroline-Norins-MacBook-Pro.local>
2013-02-28 15:22:50 +0100

    fix

--- pubguiden/application/views/list_pubs/top20.blade.php
+++ pubguiden/application/views/list_pubs/top20.blade.php
@@ -3,10 +3,8 @@
 @section('content')
 <section class="content container">
 	<h2>Top 20</h2>	
-	<ul>
-		@foreach($pubs as $pub)
-			<li> <i class="icon-map-marker"></i>{{ HTML::link_to_action("pubs@index", $pub->name, array($pub->id)) }} </li>
-		@endforeach
-	</ul>
+	<ol class="top-list">
+
+	</ol>
 </section>	
 @endsection
